8863c5c1348f5b997477fdcdb708ad82a56c5fa8
Kiara Alburqueque <kiara.alburqueque@tecsup.edu.pe>
2026-08-26 02:18:14 -0500

    Estructura inicial del proyecto y funcion main

---
+++ semana-02/Lab02CarritoKotlin/app/src/main/java/com/example/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,8 @@
+package com.example.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
+    println("=========================================")
+
+}
