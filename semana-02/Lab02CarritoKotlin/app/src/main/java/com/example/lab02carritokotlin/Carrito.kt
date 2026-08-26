fbac209f1542d1a508a03cc712af14d395448fdc
Kiara Alburqueque <kiara.alburqueque@tecsup.edu.pe>
2026-08-26 02:25:45 -0500

    Agregar data class Producto

--- semana-02/Lab02CarritoKotlin/app/src/main/java/com/example/lab02carritokotlin/Carrito.kt
+++ semana-02/Lab02CarritoKotlin/app/src/main/java/com/example/lab02carritokotlin/Carrito.kt
@@ -1,5 +1,10 @@
 package com.example.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
 fun main() {
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
