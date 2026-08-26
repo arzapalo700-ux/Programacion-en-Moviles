865cc2711762cc90026abe12eecce733aaa3467d
Kiara Alburqueque <kiara.alburqueque@tecsup.edu.pe>
2026-08-26 02:38:17 -0500

    Agrega productos iniciales al carrito

--- semana-02/Lab02CarritoKotlin/app/src/main/java/com/example/lab02carritokotlin/Carrito.kt
+++ semana-02/Lab02CarritoKotlin/app/src/main/java/com/example/lab02carritokotlin/Carrito.kt
@@ -15,4 +15,16 @@ fun main() {
 
     println("Cliente: $nombreCliente\n")
 
+    val p1 = Producto("Laptop HP", 2500.0, 1)
+    val p2 = Producto("Mouse Logitech", 45.5, 2)
+    val p3 = Producto("Audifonos Sony", 120.0, 1)
+    val p4 = Producto("USB Kingston 64GB", 25.0, 3)
+
+    carrito.add(p1)
+    carrito.add(p2)
+    carrito.add(p3)
+    carrito.add(p4)
+
+    println("Se agregaron ${carrito.size} productos al carrito con exito.\n")
+
 }
